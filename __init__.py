932d0c118d8e8f695a4f265fc319c6a58a9ffc9e
Stephen Brown <steve@fig14.com>
2016-05-20 13:53:28 +0100

    2to3

--- __init__.py
+++ __init__.py
@@ -6,8 +6,8 @@ __author__ = 'Stephen Brown (Little Fish Solutions LTD)'
 
 import logging
 
-from linegraphrender import SvgLineGraph, SvgLineGraphStyle, SvgLineGraphSeriesStyle
-from piechartrenderer import SvgPieChart, SvgPieChartStyle, SvgPieChartItemStyle
-from hbarchartrenderer import SvgHBarChart, SvgHBarChartStyle
+from .linegraphrender import SvgLineGraph, SvgLineGraphStyle, SvgLineGraphSeriesStyle
+from .piechartrenderer import SvgPieChart, SvgPieChartStyle, SvgPieChartItemStyle
+from .hbarchartrenderer import SvgHBarChart, SvgHBarChartStyle
 
 log = logging.getLogger(__name__)
---
+++ __init__.py.bak
@@ -0,0 +1,13 @@
+"""
+Package for drawing SVG graphs
+"""
+
+__author__ = 'Stephen Brown (Little Fish Solutions LTD)'
+
+import logging
+
+from linegraphrender import SvgLineGraph, SvgLineGraphStyle, SvgLineGraphSeriesStyle
+from piechartrenderer import SvgPieChart, SvgPieChartStyle, SvgPieChartItemStyle
+from hbarchartrenderer import SvgHBarChart, SvgHBarChartStyle
+
+log = logging.getLogger(__name__)
