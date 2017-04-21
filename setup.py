69d4ad78d565e32ad1564b8ffff18392ee06e321
Stephen Brown <steve@fig14.com>
2017-04-21 21:05:11 +0100

    Packaged up as library for pypi

---
+++ setup.py
@@ -0,0 +1,29 @@
+import sys
+from setuptools import setup
+
+if sys.version_info.major < 3:
+    sys.exit('Sorry, this library only supports Python 3')
+
+setup(
+    name='svggraph',
+    packages=['svggraph'],
+    version='0.0.1',
+    description='SVG graph rendering library',
+    author='Stephen Brown (Little Fish Solutions LTD)',
+    author_email='opensource@littlefish.solutions',
+    url='https://github.com/stevelittlefish/svggraph',
+    download_url='https://github.com/stevelittlefish/svggraph/archive/v0.0.1.tar.gz',
+    keywords=['svg', 'graph', 'line', 'bar', 'pie'],
+    license='Apache',
+    classifiers=[
+        'License :: OSI Approved :: Apache Software License',
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'Programming Language :: Python :: 3',
+        'Topic :: Software Development :: Libraries'
+    ],
+    install_requires=[
+        'easysvg>=0.2.1',
+    ],
+)
+
--- svggraph/__init__.py
+++ svggraph/__init__.py
@@ -7,7 +7,6 @@ import logging
 from .linegraphrender import SvgLineGraph, SvgLineGraphStyle, SvgLineGraphSeriesStyle
 from .piechartrenderer import SvgPieChart, SvgPieChartStyle, SvgPieChartItemStyle
 from .hbarchartrenderer import SvgHBarChart, SvgHBarChartStyle
-from .version import VERSION
 
 __author__ = 'Stephen Brown (Little Fish Solutions LTD)'
 
