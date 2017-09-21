3c2630292e08c63cff3ed4bf4353c0b1a84a969b
Stephen Brown <steve@fig14.com>
2017-09-21 16:03:58 +0100

    Added line graph to test app

--- testapp/main.py
+++ testapp/main.py
@@ -3,11 +3,12 @@ Main blueprint for test app
 """
 
 import logging
-import decimal
 
-from flask import Blueprint, render_template, current_app
+from flask import Blueprint, render_template
+from littlefish import colourutil
 
 import svggraph
+import testdata
 
 __author__ = 'Stephen Brown (Little Fish Solutions LTD)'
 
@@ -23,4 +24,69 @@ def index():
 
 @main.route('/line-graph')
 def line_graph():
-    return '1'
+    x_labels = []
+
+    raw_data = testdata.WIDGET_SALES_RAW[-365 * 2:]
+    avg_data = testdata.WIDGET_SALES_AVG[-365 * 2:]
+
+    raw = []
+    avg = []
+
+    for item in raw_data:
+        date = item[0]
+        if date.day == 1:
+            x_labels.append(date.strftime('%d %b %Y'))
+        else:
+            x_labels.append('')
+        
+        raw.append(item[1])
+
+    for item in avg_data:
+        avg.append(item[1])
+
+    graph_style = svggraph.SvgLineGraphStyle(
+        key_title='',
+        key_title_width=110,
+        key_title_text_size=11,
+        key_label_text_size=11,
+        key_max_rows=2,
+        key_order_in_columns=True,
+        show_minor_x_grid_lines=False,
+        x_label_rotation=45,
+        x_scale_text_height=80,
+        right_margin=80,
+        y_min_intervals=3,
+        y_minor_interval=10,
+    )
+
+    base_series_style = svggraph.SvgLineGraphSeriesStyle(
+        line_colour='#000000',
+        show_points=False,
+        show_line=False,
+        point_radius=3,
+        line_thickness=2,
+    )
+
+    line_chart = svggraph.SvgLineGraph(x_labels=x_labels, style=graph_style)
+
+    colour_raw = '#33bb22'
+    colour_avg = '#d33682'
+    
+    line_chart.add_series('Widget Sales', raw,
+                          base_series_style.clone(
+                              show_points=True,
+                              point_outline_colour=colourutil.html_color_to_rgba(colour_raw, 0.7),
+                              point_fill_colour=colourutil.html_color_to_rgba(colour_raw, 0.5),
+                              key_colour=colourutil.blend_html_colour_to_white(colour_raw, 0.6),
+                              key_shape='circle'
+                          ))
+    
+    line_chart.add_series('Rolling Average', avg,
+                          base_series_style.clone(
+                              show_line=True,
+                              line_colour=colour_avg
+                          ))
+
+    svg = line_chart.render(800, 500, view_box_mode=True)
+
+    return render_template('line_graph.html', svg=svg)
--- testapp/testdata.py
+++ testapp/testdata.py
@@ -12,12 +12,14 @@ __author__ = 'Stephen Brown (Little Fish Solutions LTD)'
 log = logging.getLogger(__name__)
 
 # List of key pairs (date, num_sold)
-WIDGET_SALES = []
+WIDGET_SALES_RAW = []
+WIDGET_SALES_AVG = []
 
 SECONDS_IN_DAY = 60 * 60 * 24
 
+
 def init():
-    global WIDGET_SALES
+    global WIDGET_SALES_RAW
     
     now = datetime.datetime.utcnow()
     date = now - datetime.timedelta(days=366 * 4)
@@ -25,11 +27,27 @@ def init():
 
     while date <= now:
         day_number = (date.timestamp() - start_timestamp) / SECONDS_IN_DAY
-        sin = math.sin(day_number / 365) + 1
+        sin = math.sin(day_number * math.pi * 2 / 365) + 1
+        print(day_number * math.pi / 365, sin)
         mult = day_number * 0.05 + 10
-        rand = random.random() * mult * 0.3
-        sales = sin * mult + rand
+        rand = random.random() * mult * 2
+        sales = sin * mult + rand + 5
         
-        WIDGET_SALES.append((date, sales))
+        WIDGET_SALES_RAW.append((date, sales))
 
         date += datetime.timedelta(days=1)
+
+    avg_width = 7
+    # try and centre it
+    before = avg_width // 2
+    for i in range(before):
+        WIDGET_SALES_AVG.append((WIDGET_SALES_RAW[i], None))
+    
+    for i in range(len(WIDGET_SALES_RAW) - avg_width + 1):
+        tot = 0
+        for j in range(i, i + avg_width):
+            tot += WIDGET_SALES_RAW[j][1]
+        
+        WIDGET_SALES_AVG.append((WIDGET_SALES_RAW[i][0], float(tot) / avg_width))
+
+
